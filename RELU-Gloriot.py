e939e20f0d2b6105c72f86140a6b0c9bd1b1456c
nishgaba-ai <nishgaba.ai@gmail.com>
2017-08-26 23:19:50 +0100

    Add files via upload
    This is a Tensor-flow implementation of ReLu(Rectified Linear Units) with Gloriot initialization

---
+++ RELU-Gloriot.py
@@ -0,0 +1,10 @@
+# DEVELOPED USING: Tensorflow, Conda Environment, NumPy, Python 2.7
+# RELU GLORIOT INITIALIZATION
+def fully_connected_layer_relu(inputs, input_dim, output_dim, nonlinearity=tf.nn.relu):
+    weights = tf.Variable(
+        tf.truncated_normal(
+            [input_dim, output_dim], stddev=2. / (input_dim + output_dim)**0.5), 
+        'weights')
+    biases = tf.Variable(tf.zeros([output_dim]), 'biases')
+    outputs = nonlinearity(tf.matmul(inputs, weights) + biases)
+    return outputs
