cfd5a18d817f9267e80fa1c78d3fbae0ebbd9b1d
nishgaba-ai <nishgaba.ai@gmail.com>
2017-10-05 22:41:21 +0100

    Create Ada-Grad.py

---
+++ Optimizers/Ada-Grad.py
@@ -0,0 +1,6 @@
+
+# AdagradOptimizer
+with tf.name_scope('train'):
+    train_step = tf.train.AdagradOptimizer(learning_rate=0.01, name='Adagrad').minimize(error)
+    
+init = tf.global_variables_initializer()
