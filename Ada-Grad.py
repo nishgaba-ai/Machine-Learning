cf87d1b987967cddc1deab11af7db17a89334beb
nishgaba-ai <nishgaba.ai@gmail.com>
2017-10-05 22:41:35 +0100

    Delete Ada-Grad.py

--- Ada-Grad.py
+++
@@ -1,5 +0,0 @@
-# AdagradOptimizer
-with tf.name_scope('train'):
-    train_step = tf.train.AdagradOptimizer(learning_rate=0.01, name='Adagrad').minimize(error)
-    
-init = tf.global_variables_initializer()
